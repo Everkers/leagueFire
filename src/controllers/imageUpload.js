f26189446a4dd5f1580cac8292ed935e7b9634d0
everkers <waelkourich12@gmail.com>
2019-12-26 15:41:41 +0100

    add error handler

--- src/controllers/imageUpload.js
+++ src/controllers/imageUpload.js
@@ -4,16 +4,20 @@ router.get('/', async (req, res) => {
 	// const deleteImage = promisify(cloudinary.uploader.destroy);
 	try {
 		const { url } = req.query;
-		const id = Math.floor(Math.random() * 1000);
-		const handlerImage = new imageHandler(url, id);
-		const response = await handlerImage.uploadImage();
-		res.status(200).send({
-			message: 'success',
-			url: response.eager[0].url,
-			id
-		});
+		if (url && url.length > 1) {
+			const id = Math.floor(Math.random() * 1000);
+			const handlerImage = new imageHandler(url, id);
+			const response = await handlerImage.uploadImage();
+			res.status(200).send({
+				message: 'success',
+				url: response.eager[0].url,
+				id
+			});
+		}
+		res.send({ message: 'there is no url' });
 	} catch (err) {
 		console.log(err);
+		res.send({ message: err.message });
 	}
 });
 module.exports = router;
